0c3f7ee9bbf38f4bf523ffc3041248e52d5fb425
Дима <@DESKTOP-J3V34PA>
2022-09-09 06:00:02 +0300

    Добавьте файлы проекта.

---
+++ Task 4/Task 4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    bool afternoon, curtain, light;
+
+    cout << "День?" << std::endl;
+    cin >> afternoon;
+    cout << "Шторы" << std::endl;
+    cin >> curtain;
+    cout << "Свет" << std::endl;
+    cin >> light;
+
+    if (afternoon and curtain == true or light == true)
+    {
+        cout << "В комнате светло." << std::endl;
+    }
+    else
+    {
+        cout << "В комнате не светло." << std::endl;
+    }
+}
